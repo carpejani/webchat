b4f85331d8bca5bb2eb85a2318ab7a4be9b33114
Jayson Carpejani <carpejani@msn.com>
2020-07-25 00:51:49 -0300

    jjjjjj

--- src/Video.js
+++ src/Video.js
@@ -22,7 +22,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Video.css";
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://video.sebastienbiollo.com' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? 'https://webchatcan.azurewebsites.net' : "http://localhost:4001"
 
 var connections = {}
 const peerConnectionConfig = {
